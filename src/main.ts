3d4f9fce26ee28ead2cb73f18c4e66b14ea82d19
Manuel Spuhler <manuel.spuhler@chuv.ch>
2021-07-14 12:32:33 +0000

    cookie-parser lib

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 const globalPrefix = '/api/v1'
 
@@ -9,6 +10,7 @@ async function bootstrap() {
   app.enableShutdownHooks();
   app.setGlobalPrefix(globalPrefix);
   app.enableCors();
+  app.use(cookieParser());
 
   const options = new DocumentBuilder()
     .setTitle('API Gateway')
