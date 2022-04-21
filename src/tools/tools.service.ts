5ea46c989d6f9db5460d9f61136a6b56a0c9baa2
Manuel Spuhler <manuel.spuhler@chuv.ch>
2022-04-21 15:50:42 +0000

    spawn docker db.create

--- src/tools/tools.service.ts
+++ src/tools/tools.service.ts
@@ -1,46 +1,75 @@
 import { Injectable } from '@nestjs/common'
 import { CreateBidsDatabaseDto } from './dto/create-bids-database.dto'
-var Docker = require('dockerode')
 
 @Injectable()
 export class ToolsService {
 
-    dbCreate =  {
-        "owner": "${USER}",
-        "database": "${DATABASE_NAME}",
-        "DatasetDescJSON": {
-            "Name": "My New BIDS db",
-            "BIDSVersion": "1.4.0",
-            "License": "n/a",
-            "Authors": [
-                "Tom",
-                "Jerry"
-            ],
-            "Acknowledgements": "Overwrite test",
-            "HowToAcknowledge": "n/a",
-            "Funding": "Picsou",
-            "ReferencesAndLinks": "n/a",
-            "DatasetDOI": "n/a"
-        }
-    }
-
-
     getBIDSDatabase() {
-        const docker = new Docker()
-        // docker run -it --rm \
-        // -v ${PROJET_TMP_FOLDER}:/input \
-        // -v ${PROJET_TMP_FOLDER}:/output \
-        // -v ${PROJECT_ROOT}/scripts:/scripts \
-        // bids-converter  \
-        // --command=db.create \
-        // --input_data=/input/db_create.json
+        const dbCreate = {
+            "owner": "www-data",
+            "database": "MY-AMAZING-DB",
+            "DatasetDescJSON": {
+                "Name": "My New BIDS db",
+                "BIDSVersion": "1.4.0",
+                "License": "n/a",
+                "Authors": [
+                    "Tom",
+                    "Jerry"
+                ],
+                "Acknowledgements": "Overwrite test",
+                "HowToAcknowledge": "n/a",
+                "Funding": "Picsou",
+                "ReferencesAndLinks": "n/a",
+                "DatasetDOI": "n/a"
+            }
+        }
+
+
+        const { spawn } = require('child_process')
+        const fs = require('fs')
+
+        try {
+            fs.writeFileSync('/home/manuel/db_create.json', JSON.stringify(dbCreate))
+        } catch (err) {
+            console.error(err)
+        }
+
+
+        const child = spawn('docker',
+            [
+                'run',
+                '-v',
+                '/home/manuel:/input',
+                '-v',
+                '/mnt/nextcloud-dp/nextcloud/data/guspuhle/files:/output',
+                '-v',
+                '/home/manuel/workdir/bids-converter/scripts:/scripts',
+                'bids-converter',
+                '--command=db.create',
+                '--input_data=/input/db_create.json'
+            ])
+
+        child.stdout.on('data', (data) => {
+            console.log(`child stdout:\n${data}`)
+        })
+
+        child.stderr.on('data', (data) => {
+            console.error(`child stderr:\n${data}`)
+        })
+
+        child.on('error', error => {
+            console.log('error', error)
+        })
+
+        child.on('close', code => {
+            console.log('closed with code', code)
+        })
+
+
+        // docker-compose -f nextcloud-docker/docker-compose.yml exec --user www-data app php occ files:scan --all
+
     }
-
-    createBIDSDatabase(createBidsDatabaseDto: CreateBidsDatabaseDto) {
-
-
-    }
-
+    createBIDSDatabase(createBidsDatabaseDto: CreateBidsDatabaseDto) { }
     getSubject() { }
     importSubject() { }
     deleteSubject() { }
