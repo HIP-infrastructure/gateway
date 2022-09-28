46468c93347f92eb079b1ef0a51cf835cd3a2e9d
Manuel Spuhler <manuel.spuhler@chuv.ch>
2022-09-28 12:01:31 +0200

    fix(groups): groups' users for non admin

--- src/groups/groups.service.ts
+++ src/groups/groups.service.ts
@@ -221,8 +221,13 @@ export class GroupsService {
 	}
 
 	async findOne(tokens: any, groupid: string): Promise<GroupDto> {
+		return this.findOne2(tokens, groupid)
+	}
+
+	async findOne1(tokens: any, groupid: string): Promise<GroupDto> {
 		const headers = {
 			...tokens,
+				'OCS-APIRequest': true,
 			accept: 'application/json, text/plain, */*',
 		}
 
@@ -232,5 +237,31 @@ export class GroupsService {
 		)
 
 		return firstValueFrom(response).then(r => r.data.ocs.data)
+	}
+
+	async findOne2(tokens: any, groupid: string): Promise<any> {
+		try {
+			const headers = {
+				...tokens,
+				'OCS-APIRequest': true,
+				accept: 'application/json, text/plain, */*',
+			}
+
+			const response = this.httpService.get(
+				`${process.env.HOSTNAME_SCHEME}://${process.env.HOSTNAME}/apps/hip/api/groupusers?groupId=${groupid}`,
+				{ headers }
+			)
+
+			return firstValueFrom(response).then(r => {
+				this.logger.debug(r.data)
+				return r.data
+			})
+		} catch (error) {
+			this.logger.debug({ error })
+			throw new HttpException(
+				error.message,
+				error.status ?? HttpStatus.BAD_REQUEST
+			)
+		}
 	}
 }
