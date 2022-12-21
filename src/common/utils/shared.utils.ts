8875a8b1a2e4c0f49a37abdd86447c1494085793
Manuel Spuhler <manuel.spuhler@chuv.ch>
2022-12-21 10:15:47 +0100

    feat: log level

---
+++ src/common/utils/shared.utils.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/no-use-before-define */
+
+import { LogLevel } from '@nestjs/common'
+
+export const LOG_LEVELS = [
+  ['warn', 'error'],
+  ['warn', 'error', 'log'],
+  ['warn', 'error', 'log', 'verbose'],
+  ['warn', 'error', 'log', 'verbose', 'debug']
+]
+
+export const getLogLevels = (level: number): LogLevel[] => {
+  let internLevel = level - 1
+  if (internLevel > LOG_LEVELS.length || internLevel < 0) internLevel = 0
+  
+  return LOG_LEVELS[internLevel] as LogLevel[]
+}
--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,25 @@ import { AppModule } from './app.module'
 import * as cookieParser from 'cookie-parser'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import { join } from 'path';
-
+import { getLogLevels } from './common/utils/shared.utils'
 
 const globalPrefix = '/api/v1'
 const publicFolder = join(__dirname, '../public')
 
+const DEFAULT_LEVEL = process.env.NODE_ENV === 'production' ? 1 : 4
+const LOG_LEVEL = process.env.LOG_LEVEL
+  ? parseInt(process.env.LOG_LEVEL)
+  : DEFAULT_LEVEL
+
 async function bootstrap() {
-	const app = await NestFactory.create<NestExpressApplication>(AppModule)
+	const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+		logger: getLogLevels(LOG_LEVEL),
+	})
 	app.enableShutdownHooks()
 	app.setGlobalPrefix(globalPrefix)
 	app.enableCors()
 	app.use(cookieParser())
-	app.useStaticAssets(publicFolder);
+	app.useStaticAssets(publicFolder)
 
 	await app.listen(4000)
 }
