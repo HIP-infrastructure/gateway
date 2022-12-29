5e861a09c95dffbde8fd567123d8608f1dbf1fcf
Manuel Spuhler <manuel.spuhler@chuv.ch>
2022-12-29 18:03:19 +0100

    feat(projects): API mockup

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,26 +1,47 @@
-import { Injectable } from '@nestjs/common';
-import { CreateProjectDto } from './dto/create-project.dto';
-import { UpdateProjectDto } from './dto/update-project.dto';
+import { Injectable } from '@nestjs/common'
+import { CreateProjectDto } from './dto/create-project.dto'
+import { UpdateProjectDto } from './dto/update-project.dto'
+import * as MOCKED_RESPONSE from '../../public/data/projects.json'
 
 @Injectable()
 export class ProjectsService {
-  create(createProjectDto: CreateProjectDto) {
-    return 'This action adds a new project';
-  }
+	/*
+	 * TODO:
+	 * Mount the Collab Filesystem
+	 */
+	constructor() {}
+
+	/*
+	 * TODO:
+	 * 1. Create a new group within iam.ebrains.eu
+   * 2. Add the owner as admin to the group
+	 * 3. Create a new folder in the Collab Filesystem
+	 */
+	create(createProjectDto: CreateProjectDto) {
+		return 'This action adds a new project'
+	}
 
   findAll() {
-    return `This action returns all projects`;
+    return MOCKED_RESPONSE
+	}
+
+  /*
+  * TODO:
+  * 1. Find all projects where the user is a member at iam.ebrains.eu
+  */
+  findUserProjects(userId: string) {
+    return MOCKED_RESPONSE.filter(p => p.status === 'active')
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} project`;
-  }
+	findOne(id: number) {
+		return `This action returns a #${id} project`
+	}
 
-  update(id: number, updateProjectDto: UpdateProjectDto) {
-    return `This action updates a #${id} project`;
-  }
+	update(id: number, updateProjectDto: UpdateProjectDto) {
+		return `This action updates a #${id} project`
+	}
 
-  remove(id: number) {
-    return `This action removes a #${id} project`;
-  }
+	remove(id: number) {
+		return `This action removes a #${id} project`
+	}
 }
