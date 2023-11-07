af178682133c6e999de2a144123ac5756ae08641
Manuel Spuhler <manuel.spuhler@chuv.ch>
2023-11-07 11:34:55 +0100

    feat: new env var IAM_REALM

--- src/config/api.iam.config.ts
+++ src/config/api.iam.config.ts
@@ -6,6 +6,7 @@ export default registerAs('iam', () => {
     clientId: process.env.IAM_CLIENT_ID || 'id',
     clientSecret: process.env.IAM_CLIENT_SECRET || 'secret',
     apiUrl: process.env.IAM_API_URL || 'https://wiki-int.ebrains.eu/rest/v1',
-    platformAdmins: process.env.PLATFORM_ADMINS?.split(',') || []
+    platformAdmins: process.env.PLATFORM_ADMINS?.split(',') || [],
+    realm: process.env.IAM_REALM || 'dev'
   }
 })
